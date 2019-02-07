e3459c3920271f45b329ef8d57767a25d8745ce2
Kashirin Ivan <ivakashirin@yandex.ru>
2019-02-07 12:38:25 +0700

    Решение Range*

--- Range/src/academit/kashirin/NumericRange.java
+++ Range/src/academit/kashirin/NumericRange.java
@@ -4,17 +4,38 @@ import java.util.Scanner;
 
 public class NumericRange {
     public static void main(String[] args) {
-        Range range1 = new Range( 54.1, 654.45 );
-        System.out.println( "Длина диапазона : " + range1.getLength() );
+        Range range1 = new Range(54.1, 654.45);
+        System.out.println("Длина диапазона : " + range1.getLength());
 
-        System.out.print( "Введите число : " );
-        Scanner scanner = new Scanner( System.in );
+        System.out.print("Введите число : ");
+        Scanner scanner = new Scanner(System.in);
         double selectedNumber = scanner.nextInt();
 
-        if (range1.isInside( selectedNumber )) {
-            System.out.print( "Число входит в диапозон" );
+        if (range1.isInside(selectedNumber)) {
+            System.out.println("Число входит в диапозон");
         } else {
-            System.out.print( "Число не входит в диапозон" );
+            System.out.println("Число не входит в диапозон");
+        }
+
+        System.out.println("Длинна диапазона = " + range1.getLength());
+
+        Range range2 = new Range(10, 900);
+        if (range1.getIntervalIntersection(range2) == null) {
+            System.out.println("Диапазон пересечения " + null);
+        } else {
+            System.out.println("Диапазон пересечения " + range1.getIntervalIntersection(range2).getFrom() + " " + range1.getIntervalIntersection(range2).getTo());
+        }
+
+        for (int i = 0; i < range1.getIntervalUnion(range2).length; i++) {
+            System.out.println("Диапазон объединеный " + range1.getIntervalUnion(range2)[i].getFrom() + " " + range1.getIntervalUnion(range2)[i].getTo());
+        }
+
+        if (range1.getIntervalDifference(range2) == null) {
+            System.out.println("Диапазон разности " + null);
+        } else {
+            for (int i = 0; i < range1.getIntervalDifference(range2).length; i++) {
+                System.out.println("Диапазон разности " + range1.getIntervalDifference(range2)[i].getFrom() + " " + range1.getIntervalDifference(range2)[i].getTo());
+            }
         }
     }
 }
@@ -50,5 +71,39 @@ class Range {
 
     boolean isInside(double selectedNumber) {
         return selectedNumber >= from && selectedNumber <= to;
+    }
+
+    public Range getIntervalIntersection(Range range2) {
+        if ((range2.from > to) || (range2.to < from)) {
+            return null;
+        } else {
+            return new Range(Math.max(from, range2.from), Math.min(to, range2.to));
+        }
+    }
+
+    public Range[] getIntervalUnion(Range range2) {
+        if ((range2.from > to) || (range2.to < from)) {
+            return new Range[]{new Range(from, to), new Range(range2.from, range2.to)};
+        } else {
+            return new Range[]{new Range(Math.min(from, range2.from), Math.max(to, range2.to))};
+        }
+    }
+
+    public Range[] getIntervalDifference(Range range2) {
+        if ((range2.from > to) || (range2.to < from)) {
+            return new Range[]{new Range(from, to)};
+        } else if (from > range2.from) {
+            if (to > range2.to) {
+                return new Range[]{new Range(range2.to, to)};
+            } else {
+                return null;
+            }
+        } else {
+            if (to <= range2.to) {
+                return new Range[]{new Range(from, range2.from)};
+            } else {
+                return new Range[]{new Range(from, range2.from), new Range(range2.to, to)};
+            }
+        }
     }
 }
