6b19c1148900ac8bee992bc7008f72268faa7db7
Kashirin Ivan <ivakashirin@yandex.ru>
2019-02-08 16:57:54 +0700

    Исправлены замечания по решению Range* 1. range2 лучше переименовать в range 2. В разности неверный результат когда отрезки равны

--- Range/src/academit/kashirin/NumericRange.java
+++ Range/src/academit/kashirin/NumericRange.java
@@ -6,7 +6,7 @@ import academit.kashirin.range.Range;
 
 public class NumericRange {
     public static void main(String[] args) {
-        Range range1 = new Range(54.1, 654.45);
+        Range range1 = new Range(69, 901);
         System.out.println("Длина диапазона : " + range1.getLength());
 
         System.out.print("Введите число : ");
@@ -21,19 +21,19 @@ public class NumericRange {
 
         System.out.println("Длинна диапазона = " + range1.getLength());
 
-        Range range2 = new Range(70, 900);
-        if (range1.getIntervalIntersection(range2) == null) {
+        Range range = new Range(70, 900);
+        if (range1.getIntervalIntersection(range) == null) {
             System.out.println("Диапазон пересечения " + null);
         } else {
-            System.out.println("Диапазон пересечения " + range1.getIntervalIntersection(range2).getFrom() + " " + range1.getIntervalIntersection(range2).getTo());
+            System.out.println("Диапазон пересечения " + range1.getIntervalIntersection(range).getFrom() + " " + range1.getIntervalIntersection(range).getTo());
         }
 
-        for (int i = 0; i < range1.getIntervalUnion(range2).length; i++) {
-            System.out.println("Диапазон объединеный " + range1.getIntervalUnion(range2)[i].getFrom() + " " + range1.getIntervalUnion(range2)[i].getTo());
+        for (int i = 0; i < range1.getIntervalUnion(range).length; i++) {
+            System.out.println("Диапазон объединеный " + range1.getIntervalUnion(range)[i].getFrom() + " " + range1.getIntervalUnion(range)[i].getTo());
         }
 
-        for (int i = 0; i < range1.getIntervalDifference(range2).length; i++) {
-            System.out.println("Диапазон разности " + range1.getIntervalDifference(range2)[i].getFrom() + " " + range1.getIntervalDifference(range2)[i].getTo());
+        for (int i = 0; i < range1.getIntervalDifference(range).length; i++) {
+            System.out.println("Диапазон разности " + range1.getIntervalDifference(range)[i].getFrom() + " " + range1.getIntervalDifference(range)[i].getTo());
         }
     }
 }
--- Range/src/academit/kashirin/range/Range.java
+++ Range/src/academit/kashirin/range/Range.java
@@ -33,36 +33,36 @@ public class Range {
         return selectedNumber >= from && selectedNumber <= to;
     }
 
-    public Range getIntervalIntersection(Range range2) {
-        if ((range2.from >= to) || (range2.to <= from)) {
+    public Range getIntervalIntersection(Range range) {
+        if ((range.from >= to) || (range.to <= from)) {
             return null;
         } else {
-            return new Range(Math.max(from, range2.from), Math.min(to, range2.to));
+            return new Range(Math.max(from, range.from), Math.min(to, range.to));
         }
     }
 
-    public Range[] getIntervalUnion(Range range2) {
-        if ((range2.from > to) || (range2.to < from)) {
-            return new Range[]{new Range(from, to), new Range(range2.from, range2.to)};
+    public Range[] getIntervalUnion(Range range) {
+        if ((range.from > to) || (range.to < from)) {
+            return new Range[]{new Range(from, to), new Range(range.from, range.to)};
         } else {
-            return new Range[]{new Range(Math.min(from, range2.from), Math.max(to, range2.to))};
+            return new Range[]{new Range(Math.min(from, range.from), Math.max(to, range.to))};
         }
     }
 
-    public Range[] getIntervalDifference(Range range2) {
-        if ((range2.from >= to) || (range2.to <= from)) {
+    public Range[] getIntervalDifference(Range range) {
+        if ((range.from >= to) || (range.to <= from)) {
             return new Range[]{new Range(from, to)};
-        } else if (from >= range2.from) {
-            if (to >= range2.to) {
-                return new Range[]{new Range(range2.to, to)};
+        } else if (from >= range.from) {
+            if (to > range.to) {
+                return new Range[]{new Range(range.to, to)};
             } else {
                 return new Range[]{};
             }
         } else {
-            if (to <= range2.to) {
-                return new Range[]{new Range(from, range2.from)};
+            if (to <= range.to) {
+                return new Range[]{new Range(from, range.from)};
             } else {
-                return new Range[]{new Range(from, range2.from), new Range(range2.to, to)};
+                return new Range[]{new Range(from, range.from), new Range(range.to, to)};
             }
         }
     }
