7f5be579e3961446343d4bce4684ac32d12144a9
Kashirin Ivan <ivakashirin@yandex.ru>
2019-02-18 17:34:19 +0700

    Решение CSV

---
+++ CSV/src/Csv.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Csv {
+    public static void main(String[] args) throws FileNotFoundException {
+        String stringInFile = "";
+        try (Scanner scanner = new Scanner(new FileInputStream("CSV\\src\\file_csv.csv"));
+             PrintWriter writer = new PrintWriter("CSV\\src\\index.html")) {
+            stringInFile += "<table>";
+            stringInFile += "<tr>";
+            stringInFile += "<td>";
+            while (scanner.hasNext()) {
+                stringInFile += scanner.nextLine();
+                stringInFile += System.lineSeparator();
+                stringInFile += " ";
+                stringInFile = stringInFile.replace(",", "</td><td>");
+                //stringInFile = stringInFile.replace("\r\n", "</td><td>");
+            }
+            //stringInFile = stringInFile.replace(",", "</td><td>");
+
+
+            stringInFile += "</td>";
+            stringInFile += "</tr>";
+            stringInFile += "</table>";
+
+            writer.println(stringInFile);
+
+        }
+    }
+}
