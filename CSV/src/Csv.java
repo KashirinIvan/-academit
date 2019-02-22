d9086635a70138f144685b188e020395d7260c5e
Kashirin Ivan <ivakashirin@yandex.ru>
2019-02-22 16:57:37 +0700

    Решение CSV

--- CSV/src/Csv.java
+++ CSV/src/Csv.java
@@ -7,30 +7,33 @@ public class Csv {
         String stringOutFile = "";
         try (Scanner scanner = new Scanner(new FileInputStream("CSV\\src\\file_csv.csv"));
              PrintWriter writer = new PrintWriter("CSV\\src\\index.html")) {
-            stringInFile += "<table border=\"1\">";
+
+            stringInFile += "<table border=1>";
             stringInFile += "<tr>";
             stringInFile += "<td>";
             while (scanner.hasNext()) {
                 stringInFile += scanner.nextLine();
-                char[] simbol = new char[stringInFile.length()];
-                for (int i = 0; i < stringInFile.length(); i++) {
-                    simbol[i] = stringInFile.charAt(i);
-                }
-                for (int i = 0; i < stringInFile.length(); i++) {
-                    if (simbol[i] == ',') {
-                        stringOutFile += "</td><td>";
-                    } else if (simbol[i] == '"') {
-                        stringOutFile += "";
-                    } else {
-                        stringOutFile += simbol[i];
-                    }
-                }
-                stringOutFile += "<br/>";
+                stringInFile += "<br/>";
             }
-            stringOutFile += "</td>";
-            stringOutFile += "</tr>";
-            stringOutFile += "</table>";
-            writer.println(stringOutFile);
+            for (int i = 0; i < stringInFile.length(); i++) {
+                if (stringInFile.charAt(i) == '"') {
+                    while ((stringInFile.charAt(i - 1) != '"' && stringInFile.charAt(i) == '"' && stringInFile.charAt(i + 1) != '"') != false) {
+                        i++;
+                        stringOutFile += stringInFile.charAt(i);
+                    }
+
+            }else if (stringInFile.charAt(i) == ',') {
+                stringOutFile += "</td><td>";
+            } else {
+                stringOutFile += stringInFile.charAt(i);
+            }
         }
+
+        stringOutFile += "</td>";
+        stringOutFile += "</tr>";
+        stringOutFile += "</table>";
+        writer.println(stringOutFile);
     }
 }
+}
+
