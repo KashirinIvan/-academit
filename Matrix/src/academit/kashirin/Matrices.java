b8debbfac1f4a66ae47ff23e3e65472b2c8813d2
IVAN KASHIRIN <ivakashirin@yandex.ru>
2019-02-24 17:46:43 +0600

    решение matrix

--- Matrix/src/academit/kashirin/Matrices.java
+++ Matrix/src/academit/kashirin/Matrices.java
@@ -7,7 +7,7 @@ public class Matrices {
     public static void main(String[] args) {
         Matrix matrix = new Matrix(4, 5);
         Matrix matrix1 = new Matrix(matrix);
-        Matrix matrix2 = new Matrix(new double[][]{{}, {}});
+        Matrix matrix2 = new Matrix(new double[][]{{2.2, 23, 43.4, 3, 5}, {23, 3.4, 5, 67, 5, 6}});
         Matrix matrix3 = new Matrix(new Vector[]{(new Vector(new double[]{4, 45, 5.6, 42, 7, 9})), (new Vector(new double[]{3, 4.5, 4, 67, 5}))});
 
         System.out.println(matrix.toString());
