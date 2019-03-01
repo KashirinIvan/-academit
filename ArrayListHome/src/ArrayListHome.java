fdefdb9974c6ff467c6e5ca7d65426528150491e
Kashirin Ivan <ivakashirin@yandex.ru>
2019-03-01 10:54:31 +0700

    Решение ArrayListHome

--- ArrayListHome/src/ArrayListHome.java
+++ ArrayListHome/src/ArrayListHome.java
@@ -14,16 +14,33 @@ public class ArrayListHome {
 
             for (int i = 0; i < list.size(); i++) {
                 Integer x = Integer.valueOf(list.get(i));
-                if (x % 2 == 0){
+                if (x % 2 == 0) {
                     list.remove(i);
-                }else{
-                    list.set(i,String.valueOf(x));
+                    i--;
+                } else {
+                    list.set(i, String.valueOf(x));
                 }
             }
             System.out.println(list.toString());
 
+            ArrayList<String> list1 = new ArrayList<>();
+            Integer x = Integer.valueOf(list.get(0));
+            list1.add(String.valueOf(x));
 
-
+            for (int i = 1; i < list.size(); i++) {
+                Integer y = Integer.valueOf(list.get(i));
+                boolean isRepeat = true;
+                for (int j = 0; j < list1.size(); j++) {
+                    if (Integer.valueOf(list1.get(j)) == y) {
+                        isRepeat= false;
+                        break;
+                    }
+                }
+                if (isRepeat){
+                    list1.add(String.valueOf(y));
+                }
+            }
+            System.out.println(list1.toString());
         }
     }
 }
