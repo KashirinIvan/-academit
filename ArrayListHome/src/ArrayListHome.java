93b7d5b5e5e14f95e3736f1da70c9503a14d11df
Kashirin Ivan <ivakashirin@yandex.ru>
2019-03-04 14:29:09 +0700

    Решение ArrayListHome

--- ArrayListHome/src/ArrayListHome.java
+++ ArrayListHome/src/ArrayListHome.java
@@ -4,43 +4,54 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ArrayListHome {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<>();
         try (Scanner scanner = new Scanner(new FileInputStream("ArrayListHome\\src\\file.txt"))) {
             while (scanner.hasNext()) {
                 list.add(scanner.nextLine());
             }
             System.out.println(list.toString());
-
-            for (int i = 0; i < list.size(); i++) {
-                Integer x = Integer.valueOf(list.get(i));
-                if (x % 2 == 0) {
-                    list.remove(i);
-                    i--;
-                } else {
-                    list.set(i, String.valueOf(x));
-                }
-            }
-            System.out.println(list.toString());
-
-            ArrayList<String> list1 = new ArrayList<>();
-            Integer x = Integer.valueOf(list.get(0));
-            list1.add(String.valueOf(x));
-
-            for (int i = 1; i < list.size(); i++) {
-                Integer y = Integer.valueOf(list.get(i));
-                boolean isRepeat = true;
-                for (int j = 0; j < list1.size(); j++) {
-                    if (Integer.valueOf(list1.get(j)) == y) {
-                        isRepeat= false;
-                        break;
-                    }
-                }
-                if (isRepeat){
-                    list1.add(String.valueOf(y));
-                }
-            }
-            System.out.println(list1.toString());
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
         }
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(10);
+        list1.add(112);
+        list1.add(3);
+        list1.add(5);
+        list1.add(16);
+
+        for (int i = 0; i < list1.size(); i++) {
+            Integer x = list1.get(i);
+            if (x % 2 == 0) {
+                list1.remove(i);
+                i--;
+            }
+        }
+        System.out.println(list1.toString());
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(1);
+        list2.add(10);
+        list2.add(112);
+        list2.add(1);
+        list2.add(16);
+        list2.add(5);
+        list2.add(10);
+        list2.add(16);
+        list2.add(3);
+        list2.add(10);
+
+        ArrayList<Integer> list3 = new ArrayList<>();
+
+        for (int i = 0; i < list2.size(); i++) {
+            boolean hasTest = list3.contains(list2.get(i));
+            if (!hasTest) {
+                list3.add(list2.get(i));
+            }
+        }
+        System.out.println(list3.toString());
     }
 }
