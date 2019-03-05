3975b6296a28efde97e097d3e0ff612a27c4d658
IVAN KASHIRIN <ivakashirin@yandex.ru>
2019-03-06 00:43:45 +0600

    решение List

--- List/src/academit/kashirin/Main.java
+++ List/src/academit/kashirin/Main.java
@@ -1,7 +1,26 @@
 package academit.kashirin;
 
+import academit.kashirin.list.SinglyLinkedList;
+
 public class Main {
     public static void main(String [] args){
-
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.setTop(6);
+        list.setTop(5);
+        list.setTop(1);
+        list.setTop(2);
+        System.out.println("Размерность списка = " + list.getListSize());
+        System.out.println("Значение по индексу 1 = " + list.getValueByIndex(1));
+        System.out.println("Значение по индексу 2 = " + list.getValueByIndex(2));
+        System.out.println("Значение по индексу 3 = " + list.getValueByIndex(3));
+        System.out.println("Значение по индексу 4 = " + list.getValueByIndex(4));
+        System.out.println("Значение по индексу 1 = " + list.getTop());
+        list.setValueByIndex(2,10);
+        System.out.println("Размерность списка = " + list.getListSize());
+        System.out.println("Значение по индексу 1 = " + list.getValueByIndex(1));
+        System.out.println("Значение по индексу 2 = " + list.getValueByIndex(2));
+        System.out.println("Значение по индексу 3 = " + list.getValueByIndex(3));
+        System.out.println("Значение по индексу 4 = " + list.getValueByIndex(4));
+        System.out.println("Значение по индексу 5 = " + list.getValueByIndex(5));
     }
 }
--- List/src/academit/kashirin/list/SinglyLinkedList.java
+++ List/src/academit/kashirin/list/SinglyLinkedList.java
@@ -4,12 +4,39 @@ public class SinglyLinkedList<T> {
     private ListItem<T> head;
     private int count;
 
+    public SinglyLinkedList() {
+    }
+
+    public void setTop(T data) {
+        ListItem<T> p = new ListItem<>(data, head);
+        head = p;
+        count++;
+    }
+
     public int getListSize() {
         return count;
     }
 
-    public T getValueFirstElement(){
+    public T getTop() {
+        return head.getData();
+    }
 
+    public T getValueByIndex(int index) {
+        ListItem<T> p = head;
+        for (int i = 0; i < index-1; i++) {
+            p = p.getNext();
+        }
+        return p.getData();
+    }
+
+    public void setValueByIndex(int index, T data) {
+        ListItem<T> p = head;
+        for (int i = 0; i < index; i++) {
+            p = p.getNext();
+        }
+        head.setData(data);
+        head.setNext(p);
+        count++;
     }
 }
  
