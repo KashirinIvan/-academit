82137ac821ddead95f05a9c41c82ddb24ecce699
Kashirin Ivan <ivakashirin@yandex.ru>
2019-03-06 17:41:22 +0700

    Решение List

--- List/src/academit/kashirin/Main.java
+++ List/src/academit/kashirin/Main.java
@@ -4,11 +4,12 @@ import academit.kashirin.list.SinglyLinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        SinglyLinkedList<Integer> list = new SinglyLinkedList();
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
         list.setTop(6);
         list.setTop(5);
         list.setTop(1);
         list.setTop(2);
+        list.invertList();
         System.out.println("Размерность списка = " + list.getListSize());
         System.out.println("Значение по индексу 1 = " + list.getValueByIndex(1));
         System.out.println("Значение по индексу 2 = " + list.getValueByIndex(2));
@@ -26,6 +27,7 @@ public class Main {
         System.out.println("Значение по индексу 1 = " + list.getValueByIndex(1));
         System.out.println("Значение по индексу 2 = " + list.getValueByIndex(2));
         System.out.println("Значение по индексу 3 = " + list.getValueByIndex(3));
+
         list.setValueByIndex(2, 9);
         System.out.println("Размерность списка = " + list.getListSize());
         System.out.println("Значение по индексу 1 = " + list.getValueByIndex(1));
@@ -41,15 +43,19 @@ public class Main {
         System.out.println("Значение по индексу 1 = " + list.getValueByIndex(1));
         System.out.println("Значение по индексу 2 = " + list.getValueByIndex(2));
         System.out.println("Значение по индексу 3 = " + list.getValueByIndex(3));
+        System.out.println("Значение по индексу 4 = " + list.getValueByIndex(4));
         list.removeTop();
         System.out.println("Размерность списка = " + list.getListSize());
         System.out.println("Значение по индексу 1 = " + list.getValueByIndex(1));
         System.out.println("Значение по индексу 2 = " + list.getValueByIndex(2));
+        System.out.println("Значение по индексу 2 = " + list.getValueByIndex(3));
+
         list.invertList();
         System.out.println("Разворот списка");
         System.out.println("Размерность списка = " + list.getListSize());
         System.out.println("Значение по индексу 1 = " + list.getValueByIndex(1));
         System.out.println("Значение по индексу 2 = " + list.getValueByIndex(2));
+        System.out.println("Значение по индексу 3 = " + list.getValueByIndex(3));
 
         SinglyLinkedList<Integer> list1 = new SinglyLinkedList<>();
         list1.copy(list);
@@ -57,6 +63,7 @@ public class Main {
         System.out.println("Размерность списка = " + list1.getListSize());
         System.out.println("Значение по индексу 1 = " + list1.getValueByIndex(1));
         System.out.println("Значение по индексу 2 = " + list1.getValueByIndex(2));
+        System.out.println("Значение по индексу 3 = " + list1.getValueByIndex(3));
 
         list.setValueByIndex(2, 55);
         System.out.println("Значение по индексу 1 = " + list1.getValueByIndex(1));
--- List/src/academit/kashirin/list/SinglyLinkedList.java
+++ List/src/academit/kashirin/list/SinglyLinkedList.java
@@ -34,24 +34,20 @@ public class SinglyLinkedList<T> {
     }
 
     public T removeByIndex(int index) {
-        /*ListItem<T> p = head;
+        ListItem<T> p = head;
         ListItem<T> prev = null;
         for (int i = 0; i < index - 1; i++) {
             prev = p;
             p = p.getNext();
         }
-        T temp = p.getData();
-        prev.setNext(p.getNext());
-        count--;*/
         T temp;
-        for (ListItem<T> p = head, prev = null; p != null; prev = p, p = p.getNext()) {
-            index--;
-
-            if(index==0){
-                temp = p.getData();
-                prev.setNext(p.getNext());
-            }
+        if (p == null) {
+            prev.setNext(null);
+        } else {
+            prev.setNext(p.getNext());
         }
+        temp = p.getData();
+        count--;
         return temp;
     }
 
@@ -74,10 +70,13 @@ public class SinglyLinkedList<T> {
     }
 
     public boolean removeNodeByValue(T data) {
-        ListItem<T> prev = null;
-        for (ListItem<T> p = head; p != null; prev = p, p = p.getNext()) {
+        for (ListItem<T> p = head, prev = null; p != null; prev = p, p = p.getNext()) {
             if (p.getData() == data) {
-                prev.setNext(p.getNext());
+                if (p == null) {
+                    prev.setNext(null);
+                } else {
+                    prev.setNext(p.getNext());
+                }
                 count--;
                 return true;
             }
@@ -93,9 +92,11 @@ public class SinglyLinkedList<T> {
     }
 
     public void invertList() {
+        int i = 2;
         for (ListItem<T> p = head; p != null; p = p.getNext()) {
             setTop(p.getData());
-            count--;
+            removeByIndex(i);
+            i++;
         }
     }
 
