7df8c94976d4f1e652e021ff8b50c23b48113a6b
IVAN KASHIRIN <ivakashirin@yandex.ru>
2019-03-26 23:36:37 +0600

    решение Tree

--- Tree/src/Main.java
+++ Tree/src/Main.java
@@ -5,6 +5,22 @@ public class Main {
         Tree<Integer> tree = new Tree<>();
 
         tree.add(4);
+        tree.add(5);
+        tree.add(3);
+        tree.add(1);
+        tree.add(23);
+        tree.add(3);
+        tree.add(71);
+        tree.add(16);
+        tree.add(34);
+        tree.add(13);
+        tree.add(17);
+        tree.add(37);
 
+        System.out.println("Поиск элемента 34 в дереве: " + tree.search(34));
+        System.out.println("Удаление элемента 1 : " + tree.remove(1));
+        System.out.println("Удаление элемента 34 : " + tree.remove(34));
+        System.out.println("Удаление элемента 16 : " + tree.remove(16));
+        System.out.println("Число элементов в дереве: " + tree.getCount());
     }
 }
--- Tree/src/academit/kashirin/tree/Tree.java
+++ Tree/src/academit/kashirin/tree/Tree.java
@@ -18,5 +18,99 @@ public class Tree<T> {
             count++;
             return;
         }
+        TreeNode<T> currentNode = root;
+        int tempCount = count;
+        while (tempCount == count) {
+            if (data.hashCode() < currentNode.getData().hashCode()) {
+                if (currentNode.getLeft() != null) {
+                    currentNode = currentNode.getLeft();
+                } else {
+                    currentNode.setLeft(new TreeNode<>(data, null, null));
+                    count++;
+                }
+            } else {
+                if (currentNode.getRight() != null) {
+                    currentNode = currentNode.getRight();
+
+                } else {
+                    currentNode.setRight(new TreeNode<>(data, null, null));
+                    count++;
+                }
+            }
+        }
+    }
+
+    public boolean search(T data) {
+        TreeNode<T> currentNode = root;
+        while (count != 0) {
+            if (data.hashCode() == currentNode.getData().hashCode()) {
+                break;
+            }
+            if (data.hashCode() < currentNode.getData().hashCode()) {
+                if (currentNode.getLeft() != null) {
+                    currentNode = currentNode.getLeft();
+                } else {
+                    return false;
+                }
+            } else {
+                if (currentNode.getRight() != null) {
+                    currentNode = currentNode.getRight();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean remove(T data) {
+        TreeNode<T> currentNode = root;
+        TreeNode<T> prevNode = null;
+        while (count != 0) {
+            if (data.hashCode() == currentNode.getData().hashCode()) {
+                if (currentNode.getLeft() == null && currentNode.getRight() == null) {
+                    if (prevNode.getLeft() == currentNode) {
+                        prevNode.setLeft(null);
+                    } else {
+                        prevNode.setRight(null);
+                    }
+                    break;
+                } else if (currentNode.getLeft() == null || currentNode.getRight() == null) {
+                    if (prevNode.getLeft() == currentNode) {
+                        if (currentNode.getLeft() != null) {
+                            prevNode.setLeft(currentNode.getLeft());
+                        } else {
+                            prevNode.setLeft(currentNode.getRight());
+                        }
+                    } else {
+                        if (currentNode.getLeft() != null) {
+                            prevNode.setRight(currentNode.getLeft());
+                        } else {
+                            prevNode.setRight(currentNode.getRight());
+                        }
+                    }
+                    break;
+                } else {
+
+                    break;
+                }
+            }
+            if (data.hashCode() < currentNode.getData().hashCode()) {
+                if (currentNode.getLeft() != null) {
+                    prevNode = currentNode;
+                    currentNode = currentNode.getLeft();
+                } else {
+                    return false;
+                }
+            } else {
+                if (currentNode.getRight() != null) {
+                    prevNode = currentNode;
+                    currentNode = currentNode.getRight();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
