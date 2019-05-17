d736cbf3cf258df6651e44dd7d7e5d137dc58927
Kashirin Ivan <ivakashirin@yandex.ru>
2019-05-17 16:54:56 +0700

    temperature

---
+++ Graph/src/Main.java
@@ -0,0 +1,17 @@
+import academit.kashirin.graph.Graph;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] array = {
+                {0, 1, 0, 0, 0, 0, 0},
+                {1, 0, 1, 1, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 1, 0, 1},
+                {0, 0, 0, 0, 0, 1, 0}
+        };
+
+        Graph graph = new Graph(array);
+    }
+}
---
+++ Graph/src/academit/kashirin/graph/Graph.java
@@ -0,0 +1,11 @@
+package academit.kashirin.graph;
+
+public class Graph {
+    private int [][] graph;
+
+    public Graph(int [][] graph){
+        this.graph = graph;
+    }
+
+
+}
